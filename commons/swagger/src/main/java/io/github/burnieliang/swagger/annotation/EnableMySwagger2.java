d76657d07522f5fbd68277489be119588787773e
liangboning <952735135@qq.com>
2019-09-21 10:59:08 +0800

    add missing files in swagger module

---
+++ commons/swagger/src/main/java/io/github/burnieliang/swagger/annotation/EnableMySwagger2.java
@@ -0,0 +1,14 @@
+package io.github.burnieliang.swagger.annotation;
+
+import io.github.burnieliang.swagger.config.SwaggerAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({SwaggerAutoConfiguration.class})
+public @interface EnableMySwagger2 {
+}
